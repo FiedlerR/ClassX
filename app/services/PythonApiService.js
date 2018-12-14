346cf302f6a084eede6d487bd4897ec59ca163c4
Filip <filipcoja@gmail.com>
2018-12-14 15:18:01 +0100

    Implemented CLI Arguments for keyFile and live mode CSS changes

--- app/services/PythonApiService.js
+++ app/services/PythonApiService.js
@@ -104,9 +104,8 @@ angular.module('ClassX').service('KeyService', function() {
     var keys;
     var _readKeys = () => {
         let fs = require('fs');
-        keys = JSON.parse(fs.readFileSync("./app/config/.keys", 'utf8'));
+        keys = JSON.parse(fs.readFileSync(keyPath, 'utf8'));
     };
-
     var _get = (key) => {
         if(!keys) {
             _readKeys();
