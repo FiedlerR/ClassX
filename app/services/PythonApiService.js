77a3814dcb6c3707b7f7ce76140a304f1d2fc202
yEinFallsLos <andreas.sch4@gmail.com>
2018-12-16 17:53:00 +0100

    Outsourced KeyService

--- app/services/PythonApiService.js
+++ app/services/PythonApiService.js
@@ -99,26 +99,3 @@ angular.module('ClassX').service('PythonApiService', [ "$interval", function($in
         unsubscribe: _unsubscribe
     });
 }]);
-
-angular.module('ClassX').service('KeyService', function() {
-    var keys;
-    var _readKeys = () => {
-        let fs = require('fs');
-        keys = JSON.parse(fs.readFileSync(keyPath, 'utf8'));
-    };
-    var _get = (key) => {
-        if(!keys) {
-            _readKeys();
-        }
-
-        if(key) {
-            return keys[key];
-        } else {
-            return keys;
-        }
-    };
-
-    return ({
-        get: _get
-    });
-});
